9a3712f5f57479a5bb9fdd465cd8ec9f6957a797
swaldecker <seth@paradigms.net>
2016-11-26 18:38:24 -0500

    added comment to plot1.R explaining that I checked for missing values before proceeding

--- plot1.R
+++ plot1.R
@@ -10,6 +10,10 @@ variables <- read.table( file1, header = TRUE, nrows = 1, sep = ";" )
 variables <- names(variables)
 power_df <- read.table( file1, skip = 66637, nrows = 2880, sep = ";", col.names = variables, na.strings = "?" )
 
+#NOTE: before proceeding, I checked for any NA values using 
+# apply(power_df, 2, function(x) any(is.na(x))). There were 
+# no missing values in this subset. 
+
 # Create the plot
 hist(power_df$Global_active_power, yaxt = 'n', col = "red", main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)" )
